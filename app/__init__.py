9ccc4be10407e70dfa5e38e053359c7708677d8d
brutorian <35403368+brutorian@users.noreply.github.com>
2019-07-23 16:39:34 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ from flask_bcrypt import Bcrypt
 import psycopg2
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SECRET_KEY'
-app.config['DATABASE_URL'] = 'DATABASE_URL'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)#SQL
